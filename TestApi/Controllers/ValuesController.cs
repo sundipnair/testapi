16ce922754ff1c960b5ec3e11c1fcee92fa432cf
Sundip Nair <sundip.nair@bjss.com>
2018-09-27 10:11:48 +0100

    first to git

---
+++ TestApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            GetDataAsync();
+            return new string[] { "value1", "value2" };
+        }
+
+        private async Task GetDataAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //var result = await client.GetAsync("https://services.odata.org/TripPinRESTierService/People");
+                var result = await client.GetAsync("http://test-testgo:8080/go-docker");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
